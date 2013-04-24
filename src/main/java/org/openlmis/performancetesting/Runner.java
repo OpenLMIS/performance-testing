fd98d6135177238e3d9c18ce3a468f2838c02df1
Rahul Joshi <rahulj@thoughtworks.com>
2013-04-24 13:10:44 +0530

    added facility and all its reference data insert queries, randomized all fields for all inserts

--- src/main/java/org/openlmis/performancetesting/Runner.java
+++ src/main/java/org/openlmis/performancetesting/Runner.java
@@ -1,26 +1,31 @@
+/*
+ * Copyright © 2013 VillageReach. All Rights Reserved. This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+ *
+ * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 package org.openlmis.performancetesting;
 
-import org.openlmis.core.domain.DosageUnit;
-import org.openlmis.core.domain.Product;
-import org.openlmis.core.domain.ProductCategory;
-import org.openlmis.core.domain.ProductForm;
+import org.openlmis.core.domain.*;
+import org.openlmis.performancetesting.dao.FacilityDAO;
 import org.openlmis.performancetesting.dao.ProductDAO;
+import org.openlmis.performancetesting.helper.FacilityHelper;
 import org.openlmis.performancetesting.helper.ProductHelper;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Runner {
 
-  ProductHelper productHelper;
+  ProductHelper productHelper = new ProductHelper();
+  FacilityHelper facilityHelper = new FacilityHelper();
+
   ProductDAO productDAO;
+  FacilityDAO facilityDAO;
 
   public Runner() {
     ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext-performance.xml");
     productDAO = (ProductDAO) ctx.getBean("productDAO");
-
-    productHelper = new ProductHelper();
-
-
+    facilityDAO = (FacilityDAO) ctx.getBean("facilityDAO");
   }
 
 
@@ -30,8 +35,33 @@ public class Runner {
   }
 
   private void insertData() {
+    insertProductData();
+    insertFacilityData();
 
 
+  }
+
+  private Facility insertFacilityData() {
+
+    GeographicLevel geoLevel = facilityHelper.createGeoLevel();
+    facilityDAO.insertGeoLevel(geoLevel);
+
+    GeographicZone geoZone = facilityHelper.createGeographicZone(geoLevel, new GeographicZone());
+    facilityDAO.insertGeoZone(geoZone);
+
+    FacilityType facilityType = facilityHelper.createFacilityType();
+    facilityDAO.insertFacilityType(facilityType);
+
+    FacilityOperator facilityOperator = facilityHelper.createFacilityOperator();
+
+    Facility facility = facilityHelper.createFacility(geoZone, facilityType, facilityOperator);
+    facilityDAO.insertFacility(facility);
+
+    System.out.println(facility.getId());
+    return facility;
+  }
+
+  private void insertProductData() {
     ProductForm productForm = productHelper.createForm();
     productDAO.insertProductForm(productForm);
 
@@ -42,10 +72,9 @@ public class Runner {
     productDAO.insertCategory(category);
 
     Product product = productHelper.createProduct(productForm, dosageUnit, category);
-    long status = productDAO.insertProduct(product);
+    long productId = productDAO.insertProduct(product);
 
-    System.out.println(status);
-
+    System.out.println(productId);
   }
 
 }
--- src/main/java/org/openlmis/performancetesting/helper/ProductHelper.java
+++ src/main/java/org/openlmis/performancetesting/helper/ProductHelper.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright © 2013 VillageReach.  All Rights Reserved.  This Source Code Form is subject to the terms of the Mozilla Public License, v. 20.
+ * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 package org.openlmis.performancetesting.helper;
 
 import org.openlmis.core.domain.DosageUnit;
@@ -5,83 +10,92 @@ import org.openlmis.core.domain.Product;
 import org.openlmis.core.domain.ProductCategory;
 import org.openlmis.core.domain.ProductForm;
 
+import java.util.Random;
+
+import static org.apache.commons.lang.RandomStringUtils.randomAlphanumeric;
+import static org.apache.commons.lang.RandomStringUtils.randomNumeric;
+import static org.apache.commons.lang.math.RandomUtils.nextBoolean;
+
 public class ProductHelper {
   public Product createProduct(ProductForm form, DosageUnit dosageUnit, ProductCategory category) {
     Product product = new Product();
-    product.setCode("SOME CODE ");
-    product.setAlternateItemCode("alternative");
-    product.setManufacturer("manu");
-    product.setManufacturerCode("manu code");
-    product.setManufacturerBarCode("bar");
-    product.setMohBarCode("mohbar");
-    product.setGtin("gtin");
-    product.setType("tty");
-    product.setDisplayOrder(999);
-    product.setPrimaryName("pname");
-    product.setFullName("full");
-    product.setGenericName("generic");
-    product.setAlternateName("aname");
-    product.setDescription("desc");
-    product.setStrength("str");
+    product.setCode(randomAlphanumeric(40));
+    product.setAlternateItemCode(randomAlphanumeric(15));
+    product.setManufacturer(randomAlphanumeric(80));
+    product.setManufacturerCode(randomAlphanumeric(30));
+    product.setManufacturerBarCode(randomAlphanumeric(18));
+    product.setMohBarCode(randomAlphanumeric(20));
+    product.setGtin(randomAlphanumeric(10));
+    product.setType(randomAlphanumeric(60));
+    product.setDisplayOrder(Integer.valueOf(randomNumeric(3)));
+    product.setPrimaryName(randomAlphanumeric(120));
+    product.setFullName(randomAlphanumeric(200));
+    product.setGenericName(randomAlphanumeric(80));
+    product.setAlternateName(randomAlphanumeric(60));
+    product.setDescription(randomAlphanumeric(100));
+    product.setStrength(randomAlphanumeric(13));
 
     product.setForm(form);
     product.setDosageUnit(dosageUnit);
     product.setCategory(category);
 
-    product.setDispensingUnit("disp-unit");
-    product.setDosesPerDispensingUnit(-1);
-    product.setPackSize(2323);
-    product.setAlternatePackSize(3232);
-    product.setStoreRefrigerated(false);
-    product.setStoreRoomTemperature(true);
-    product.setHazardous(false);
-    product.setFlammable(true);
-    product.setControlledSubstance(false);
-    product.setLightSensitive(true);
-    product.setApprovedByWHO(false);
-    product.setContraceptiveCYP(0.1);
-    product.setPackLength(1.1);
-    product.setPackWidth(2.2);
-    product.setPackHeight(3.3);
-    product.setPackWeight(4.4);
-    product.setPacksPerCarton(5);
-    product.setCartonLength(6.6);
-    product.setCartonWidth(7.7);
-    product.setCartonHeight(8.8);
-    product.setCartonsPerPallet(9);
-    product.setExpectedShelfLife(10);
-    product.setSpecialStorageInstructions("sp-store");
-    product.setSpecialTransportInstructions("sp-trf");
-    product.setActive(true);
-    product.setFullSupply(false);
-    product.setTracer(true);
-    product.setRoundToZero(false);
-    product.setArchived(true);
-    product.setPackRoundingThreshold(11);
-    product.setModifiedBy(12);
+    product.setDispensingUnit(randomAlphanumeric(20));
+    product.setDosesPerDispensingUnit(Integer.valueOf(randomNumeric(4)));
+    product.setPackSize(Integer.valueOf(randomNumeric(4)));
+    product.setAlternatePackSize(Integer.valueOf(randomNumeric(4)));
+    product.setStoreRefrigerated(nextBoolean());
+    product.setStoreRoomTemperature(nextBoolean());
+    product.setHazardous(nextBoolean());
+    product.setFlammable(nextBoolean());
+    product.setControlledSubstance(nextBoolean());
+    product.setLightSensitive(nextBoolean());
+    product.setApprovedByWHO(nextBoolean());
+    product.setContraceptiveCYP(new Random().nextDouble() * 1000);
+    product.setPackLength(new Random().nextDouble() * 1000);
+    product.setPackWidth(new Random().nextDouble() * 1000);
+    product.setPackHeight(new Random().nextDouble() * 1000);
+    product.setPackWeight(new Random().nextDouble() * 1000);
+    product.setPacksPerCarton(Integer.valueOf(randomNumeric(4)));
+    product.setCartonLength(new Random().nextDouble() * 1000);
+    product.setCartonWidth(new Random().nextDouble() * 1000);
+    product.setCartonHeight(new Random().nextDouble() * 1000);
+    product.setCartonsPerPallet(Integer.valueOf(randomNumeric(4)));
+    product.setExpectedShelfLife(Integer.valueOf(randomNumeric(4)));
+    product.setSpecialStorageInstructions(randomAlphanumeric(100));
+    product.setSpecialTransportInstructions(randomAlphanumeric(100));
+    product.setActive(nextBoolean());
+    product.setFullSupply(nextBoolean());
+    product.setTracer(nextBoolean());
+    product.setRoundToZero(nextBoolean());
+    product.setArchived(nextBoolean());
+    product.setPackRoundingThreshold(Integer.valueOf(randomNumeric(3)));
+    product.setModifiedBy(Integer.valueOf(randomNumeric(5)));
 
     return product;
   }
 
   public ProductForm createForm() {
     ProductForm form = new ProductForm();
-    form.setCode("SOME FORM CODE");
-    form.setDisplayOrder(123);
+    form.setCode(randomAlphanumeric(20));
+    form.setDisplayOrder(Integer.parseInt(randomNumeric(3)));
+
     return form;
   }
 
   public DosageUnit createDosageUnit() {
     DosageUnit dosageUnit = new DosageUnit();
-    dosageUnit.setCode("ABCD");
-    dosageUnit.setDisplayOrder(932);
+    dosageUnit.setCode(randomAlphanumeric(20));
+    dosageUnit.setDisplayOrder(Integer.parseInt(randomNumeric(3)));
+
     return dosageUnit;
   }
 
   public ProductCategory createCategory() {
     ProductCategory category = new ProductCategory();
-    category.setCode("CATEGORY ABC123CYZ CATEGORY-CATEGORY");
-    category.setDisplayOrder(435);
-    category.setName("CATEGORY NAMEEEEEEEEWERWERWW");
+    category.setCode(randomAlphanumeric(50));
+    category.setName(randomAlphanumeric(80));
+    category.setDisplayOrder(Integer.valueOf(randomNumeric(3)));
+
     return category;
   }
 }
