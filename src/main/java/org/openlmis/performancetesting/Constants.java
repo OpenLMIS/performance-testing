e67e5f246f89204987b865a8a44d9f21580f1d4c
Rahul Joshi <rahulj@thoughtworks.com>
2013-05-06 17:25:32 +0530

    |#694|+Vinay| reducing the numbers

--- src/main/java/org/openlmis/performancetesting/Constants.java
+++ src/main/java/org/openlmis/performancetesting/Constants.java
@@ -8,10 +8,10 @@ package org.openlmis.performancetesting;
 
 public class Constants {
 
-  public static final int STATES_PER_COUNTRY = 35; // 35
-  public static final int DISTRICT_PER_STATE = 25; // 25
-  public static final int FACILITIES_PER_DISTRICT = 28; // 28
-  public static final int FACILITIES_PER_REQUISITION_GROUP = 100; // 100
+  public static final int STATES_PER_COUNTRY = 10; // 35
+  public static final int DISTRICT_PER_STATE = 10; // 25
+  public static final int FACILITIES_PER_DISTRICT = 2; // 28
+  public static final int FACILITIES_PER_REQUISITION_GROUP = 10; // 100
   public static final int NO_OF_FACILITY_TYPES = 5;
   public static final int NUMBER_OF_PERIODS = 12;
   public static final int STARTING_PERIOD_MONTH = 10;
@@ -19,12 +19,12 @@ public class Constants {
 
   public static final String[] PROGRAM_NAMES = {"ESS MEDICINES", "TB", "MALARIA", "ARV/ART", "VACCINES"};
 
-  public static final int MAX_FULL_SUPPLY_COUNT = 400;     //400
-  public static final int NON_FULL_SUPPLY_COUNT = 100;     //100
-  public static final int AVERAGE_FULL_SUPPLY_COUNT = 35;  //35
-  public static final int TOTAL_NO_OF_PRODUCTS = 2000;      //2000
+  public static final int MAX_FULL_SUPPLY_COUNT = 40;     //400
+  public static final int AVERAGE_FULL_SUPPLY_COUNT = 10;  //35
+  public static final int NUMBER_OF_NON_FULL_SUPPLY_ITEMS = 10;
+  public static final int NON_FULL_SUPPLY_COUNT = 10;     //100
+
+  public static final int TOTAL_NO_OF_PRODUCTS = 200;      //2000
 
   public static final String[] ADJUSTMENT_NAMES = {"TRANSFER_IN", "TRANSFER_OUT", "DAMAGED", "LOST", "STOLEN", "EXPIRED", "PASSED_OPEN_VIAL_TIME_LIMIT", "COLD_CHAIN_FAILURE", "CLINIC_RETURN"};
-
-  public static final int NUMBER_OF_NON_FULL_SUPPLY_ITEMS = 10;
 }
