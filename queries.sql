17793580e9339ca4de0cb2b3e1cb3888730002d7
Manjyot <manjyots@thoughtworks.com>
2013-05-22 15:01:46 +0530

    CSV generators for 10 mins load

--- queries.sql
+++ queries.sql
@@ -9,7 +9,7 @@ AND R.name = 'Store In-Charge'
 AND facilityId IN (
 SELECT Id  FROM facilities WHERE Id NOT IN(
 SELECT DISTINCT(facilityId) FROM requisitions WHERE periodId = 13))
-ORDER BY U.facilityId, RA.programId  LIMIT 150;
+ORDER BY U.facilityId, RA.programId  LIMIT 375;
 " > readytoinitiate1.csv;
 
 psql -d open_lmis -U postgres -t -A -F"," -c "
@@ -21,20 +21,8 @@ AND R.name = 'Store In-Charge'
 AND facilityId IN (
 SELECT Id  FROM facilities WHERE Id NOT IN(
 SELECT DISTINCT(facilityId) FROM requisitions WHERE periodId = 13))
-ORDER BY U.facilityId, RA.programId  LIMIT 150 OFFSET 150;
+ORDER BY U.facilityId, RA.programId  LIMIT 375 OFFSET 375;
 " > readytoinitiate2.csv;
-
-psql -d open_lmis -U postgres -t -A -F"," -c "
-SELECT U.username,U.facilityId, RA.programId, 13
-FROM users U JOIN role_assignments RA ON U.id = RA.userid
-JOIN roles R ON RA.roleId = R.id
-WHERE RA.supervisoryNodeId IS NULL
-AND R.name = 'Store In-Charge'
-AND facilityId IN (
-SELECT Id  FROM facilities WHERE Id NOT IN(
-SELECT DISTINCT(facilityId) FROM requisitions WHERE periodId = 13))
-ORDER BY U.facilityId, RA.programId  LIMIT 150 OFFSET 300;
-" > readytoinitiate3.csv;
 
 
 
@@ -45,7 +33,7 @@ SELECT username, facilityid, programid, 13 FROM (
 	select U.username as username, R.facilityid, R.programid
 	from requisitions R, users U
 	where R.periodid=13 and R.status='INITIATED' and U.facilityid=R.facilityid
-	order by U.facilityid, R.programid,U.id) AS Q ) AS foo where rownumber%2=1 LIMIT 500;
+	order by U.facilityid, R.programid,U.id) AS Q ) AS foo where rownumber%2=1 LIMIT 1250;
 	" > submission1.csv;
 
 psql -d open_lmis -U postgres -t -A -F"," -c "
@@ -54,17 +42,9 @@ SELECT ROW_NUMBER() OVER (ORDER BY facilityid,programid) AS rownumber,username,f
 select U.username as username, R.facilityid, R.programid
 from requisitions R, users U
 where R.periodid=13 and R.status='INITIATED' and U.facilityid=R.facilityid
-order by U.facilityid, R.programid,U.id) AS Q ) AS foo where rownumber%2=1 LIMIT 500 OFFSET 500;
+order by U.facilityid, R.programid,U.id) AS Q ) AS foo where rownumber%2=1 LIMIT 1250 OFFSET 1250;
 " > submission2.csv;
 
-psql -d open_lmis -U postgres -t -A -F"," -c "
-SELECT username, facilityid, programid, 13 FROM (
-  SELECT ROW_NUMBER() OVER (ORDER BY facilityid,programid) AS rownumber,username,facilityid,programid from (
-    select U.username as username, R.facilityid, R.programid
-    from requisitions R, users U
-    where R.periodid=13 and R.status='INITIATED' and U.facilityid=R.facilityid
-    order by U.facilityid, R.programid,U.id) AS Q ) AS foo where rownumber%2=1 LIMIT 500 OFFSET 1000;
-    " > submission3.csv;
 
 -- READY for authorization ---------
 psql -d open_lmis -U postgres -t -A -F"," -c "
@@ -73,7 +53,7 @@ SELECT username, facilityid, programid, 13 FROM (
 	select U.username as username, R.facilityid, R.programid
 	from requisitions R, users U
 	where R.periodid=13 and R.status='SUBMITTED' and U.facilityid=R.facilityid
-	order by U.facilityid, R.programid,U.id) AS Q ) AS foo where rownumber%2=0 LIMIT 50;
+	order by U.facilityid, R.programid,U.id) AS Q ) AS foo where rownumber%2=0 LIMIT 125;
 	" > authorization1.csv;
 
 	psql -d open_lmis -U postgres -t -A -F"," -c "
@@ -82,17 +62,9 @@ SELECT username, facilityid, programid, 13 FROM (
   	select U.username as username, R.facilityid, R.programid
   	from requisitions R, users U
   	where R.periodid=13 and R.status='SUBMITTED' and U.facilityid=R.facilityid
-  	order by U.facilityid, R.programid,U.id) AS Q ) AS foo where rownumber%2=0 LIMIT 50 OFFSET 50;
+  	order by U.facilityid, R.programid,U.id) AS Q ) AS foo where rownumber%2=0 LIMIT 125 OFFSET 125;
   	" > authorization2.csv;
 
-  	psql -d open_lmis -U postgres -t -A -F"," -c "
-  	SELECT username, facilityid, programid, 13 FROM (
-      SELECT ROW_NUMBER() OVER (ORDER BY facilityid,programid) AS rownumber,username,facilityid,programid from (
-    	select U.username as username, R.facilityid, R.programid
-    	from requisitions R, users U
-    	where R.periodid=13 and R.status='SUBMITTED' and U.facilityid=R.facilityid
-    	order by U.facilityid, R.programid,U.id) AS Q ) AS foo where rownumber%2=0 LIMIT 50 OFFSET 100;
-    	" > authorization3.csv;
 
 
 -- READY for first approval -------
@@ -104,7 +76,7 @@ JOIN supervisory_nodes SN ON RG.supervisoryNodeId = SN.id
 JOIN role_assignments RA ON SN.id = RA.supervisoryNodeId
 JOIN roles RO on RO.id = RA.roleId
 JOIN users U on U.id = RA.userid
-AND RO.name='LMU' and R.periodId=13 and R.status = 'AUTHORIZED' AND RA.programId=R.programId ORDER BY R.facilityId, R.programId LIMIT 50;
+AND RO.name='LMU' and R.periodId=13 and R.status = 'AUTHORIZED' AND RA.programId=R.programId ORDER BY R.facilityId, R.programId LIMIT 125;
 " > firstapproval1.csv;
 
 psql -d open_lmis -U postgres -t -A -F"," -c "
@@ -115,19 +87,9 @@ JOIN supervisory_nodes SN ON RG.supervisoryNodeId = SN.id
 JOIN role_assignments RA ON SN.id = RA.supervisoryNodeId
 JOIN roles RO on RO.id = RA.roleId
 JOIN users U on U.id = RA.userid
-AND RO.name='LMU' and R.periodId=13 and R.status = 'AUTHORIZED' AND RA.programId=R.programId ORDER BY R.facilityId, R.programId LIMIT 50 OFFSET 50;
+AND RO.name='LMU' and R.periodId=13 and R.status = 'AUTHORIZED' AND RA.programId=R.programId ORDER BY R.facilityId, R.programId LIMIT 125 OFFSET 125;
 " > firstapproval2.csv;
 
-psql -d open_lmis -U postgres -t -A -F"," -c "
-SELECT U.username, R.facilityId, R.programId, 13 AS periodId, R.id as requisitionId
-from requisitions R JOIN requisition_group_members RGM ON R.facilityId = RGM.facilityId
-JOIN requisition_groups RG ON RGM.requisitionGroupId = RG.id
-JOIN supervisory_nodes SN ON RG.supervisoryNodeId = SN.id
-JOIN role_assignments RA ON SN.id = RA.supervisoryNodeId
-JOIN roles RO on RO.id = RA.roleId
-JOIN users U on U.id = RA.userid
-AND RO.name='LMU' and R.periodId=13 and R.status = 'AUTHORIZED' AND RA.programId=R.programId ORDER BY R.facilityId, R.programId LIMIT 150 OFFSET 100;
-" > firstapproval3.csv;
 
 -- READY for second approval ------
 psql -d open_lmis -U postgres -t -A -F"," -c "
@@ -136,7 +98,7 @@ from requisitions R JOIN supervisory_nodes SN ON R.supervisoryNodeId = SN.id
 JOIN role_assignments RA ON SN.id = RA.supervisoryNodeId
 JOIN roles RO on RO.id = RA.roleId
 JOIN users U on U.id = RA.userid
-AND RO.name='Medical-Officer' and R.periodId=13 and R.status = 'IN_APPROVAL' AND RA.programId=R.programId ORDER BY R.facilityId, R.programId LIMIT 50;
+AND RO.name='Medical-Officer' and R.periodId=13 and R.status = 'IN_APPROVAL' AND RA.programId=R.programId ORDER BY R.facilityId, R.programId LIMIT 125;
 " > secondapproval1.csv;
 
 psql -d open_lmis -U postgres -t -A -F"," -c "
@@ -145,17 +107,9 @@ from requisitions R JOIN supervisory_nodes SN ON R.supervisoryNodeId = SN.id
 JOIN role_assignments RA ON SN.id = RA.supervisoryNodeId
 JOIN roles RO on RO.id = RA.roleId
 JOIN users U on U.id = RA.userid
-AND RO.name='Medical-Officer' and R.periodId=13 and R.status = 'IN_APPROVAL' AND RA.programId=R.programId ORDER BY R.facilityId, R.programId LIMIT 50 OFFSET 50;
+AND RO.name='Medical-Officer' and R.periodId=13 and R.status = 'IN_APPROVAL' AND RA.programId=R.programId ORDER BY R.facilityId, R.programId LIMIT 125 OFFSET 125;
 " > secondapproval2.csv;
 
-psql -d open_lmis -U postgres -t -A -F"," -c "
-SELECT U.username, R.facilityId, R.programId, 13 AS periodId, R.id as requisitionId
-from requisitions R JOIN supervisory_nodes SN ON R.supervisoryNodeId = SN.id
-JOIN role_assignments RA ON SN.id = RA.supervisoryNodeId
-JOIN roles RO on RO.id = RA.roleId
-JOIN users U on U.id = RA.userid
-AND RO.name='Medical-Officer' and R.periodId=13 and R.status = 'IN_APPROVAL' AND RA.programId=R.programId ORDER BY R.facilityId, R.programId LIMIT 50 OFFSET 100;
-" > secondapproval3.csv;
 
 
 -- View Rnr ---------------
@@ -167,7 +121,7 @@ JOIN supervisory_nodes SN ON RG.supervisoryNodeId = SN.id
 JOIN role_assignments RA ON SN.id = RA.supervisoryNodeId
 JOIN roles RO on RO.id = RA.roleId
 JOIN users U on U.id = RA.userid
-AND RO.name='LMU' and R.periodId=13 and R.status = 'AUTHORIZED' AND RA.programId=R.programId ORDER BY R.facilityId, R.programId LIMIT 50 OFFSET 150;
+AND RO.name='LMU' and R.periodId=13 and R.status = 'AUTHORIZED' AND RA.programId=R.programId ORDER BY R.facilityId, R.programId LIMIT 250 OFFSET 250;
 " > viewrnr1.csv
 
 psql -d open_lmis -U postgres -t -A -F"," -c "
@@ -178,19 +132,9 @@ JOIN supervisory_nodes SN ON RG.supervisoryNodeId = SN.id
 JOIN role_assignments RA ON SN.id = RA.supervisoryNodeId
 JOIN roles RO on RO.id = RA.roleId
 JOIN users U on U.id = RA.userid
-AND RO.name='LMU' and R.periodId=13 and R.status = 'AUTHORIZED' AND RA.programId=R.programId ORDER BY R.facilityId, R.programId LIMIT 50 OFFSET 200;
+AND RO.name='LMU' and R.periodId=13 and R.status = 'AUTHORIZED' AND RA.programId=R.programId ORDER BY R.facilityId, R.programId LIMIT 250 OFFSET 500;
 " > viewrnr2.csv
 
-psql -d open_lmis -U postgres -t -A -F"," -c "
-SELECT U.username, R.facilityId, R.programId, 13
-from requisitions R JOIN requisition_group_members RGM ON R.facilityId = RGM.facilityId
-JOIN requisition_groups RG ON RGM.requisitionGroupId = RG.id
-JOIN supervisory_nodes SN ON RG.supervisoryNodeId = SN.id
-JOIN role_assignments RA ON SN.id = RA.supervisoryNodeId
-JOIN roles RO on RO.id = RA.roleId
-JOIN users U on U.id = RA.userid
-AND RO.name='LMU' and R.periodId=13 and R.status = 'AUTHORIZED' AND RA.programId=R.programId ORDER BY R.facilityId, R.programId LIMIT 50 OFFSET 250;
-" > viewrnr3.csv
 
 -- Convert to order ------------------
 psql -d open_lmis -U postgres -t -A -F"," -c "
