536b465a4b76398cfddc85b5e7440175ef1df6f4
Rahul Joshi <rahulj@thoughtworks.com>
2013-06-04 15:54:58 +0530

    updating submission csv queries to return distinct user with create privilege

--- queries.sh
+++ queries.sh
@@ -24,13 +24,36 @@ do
  offset=`expr $limit \* $counter`
 
 psql -d open_lmis -U postgres -t -A -F"," -c "
-SELECT username, facilityid, programId, programId+2, 13 FROM (
-  SELECT ROW_NUMBER() OVER (ORDER BY facilityid,programid) AS rownumber,username,facilityid,programid from (
-	select U.username as username, R.facilityid, R.programid
-	from requisitions R, users U
-	where R.periodid=13 and R.status='INITIATED' and U.facilityid=R.facilityid AND R.programId in (1,2)
-	order by U.facilityid, R.programid,U.id) AS Q ) AS foo
-	WHERE rownumber%2=1
+
+SELECT Q1.username, Q1.facilityId, Q1.programId, Q2.programId, 13
+	FROM (
+		SELECT U.username, R.facilityId, R.programId
+		FROM requisitions R, users U, roles RO, role_rights RR, role_assignments RA WHERE
+		R.periodId = 13 AND
+		R.status  = 'INITIATED' AND
+		RR.rightName = 'CREATE_REQUISITION' AND
+		RR.roleId = RO.id AND
+		RA.roleId = RO.id AND
+		RA.userId = U.id AND
+		RA.supervisoryNodeId is NULL AND
+		U.facilityId = R.facilityId AND
+		RA.programId = R.programId AND
+		R.programId %2 = 0
+		) AS Q1 JOIN (
+		SELECT U.username, R.facilityId, R.programId
+		FROM requisitions R, users U, roles RO, role_rights RR, role_assignments RA WHERE
+		R.periodId = 13 AND
+		R.status  = 'INITIATED' AND
+		RR.rightName = 'CREATE_REQUISITION' AND
+		RR.roleId = RO.id AND
+		RA.roleId = RO.id AND
+		RA.userId = U.id AND
+		RA.supervisoryNodeId is NULL AND
+		U.facilityId = R.facilityId AND
+		RA.programId = R.programId AND
+		R.programId %2 = 1
+		) AS Q2 ON Q1.facilityId = Q2.facilityId AND Q1.programId = Q2.programId+1
+		ORDER BY Q1.facilityId, Q1.programId, Q2.programId
  LIMIT $limit OFFSET $offset"  > submission`expr $counter + 1`.csv;
 done
 
@@ -76,7 +99,7 @@ do
  offset=`expr $limit \* $counter`
 
   psql -d open_lmis -U postgres -t -A -F"," -c "
-    SELECT Q1.username, Q1.facilityId, Q1.programId, Q1.requisitionId, Q2.facilityId, Q2.programId, Q2.requisitionId FROM (
+    SELECT Q1.username, Q1.programId, Q1.requisitionId, Q2.programId, Q2.requisitionId FROM (
   	SELECT U.username, R.facilityId, R.programId, R.id as requisitionId
   	from requisitions R JOIN supervisory_nodes SN ON R.supervisoryNodeId = SN.id
   	JOIN role_assignments RA ON SN.id = RA.supervisoryNodeId
@@ -84,9 +107,8 @@ do
   	JOIN users U on U.id = RA.userid
   	AND RO.name='LMU' and R.periodId=13 and R.status = 'AUTHORIZED' AND RA.programId=R.programId AND R.id %2 = 0
   	ORDER BY R.facilityId, R.programId
-  	) AS Q1
-  	 JOIN
-  	 (SELECT U.username, R.facilityId, R.programId, R.id as requisitionId
+  	) AS Q1 JOIN (
+  	SELECT U.username, R.facilityId, R.programId, R.id as requisitionId
   	from requisitions R JOIN supervisory_nodes SN ON R.supervisoryNodeId = SN.id
   	JOIN role_assignments RA ON SN.id = RA.supervisoryNodeId
   	JOIN roles RO on RO.id = RA.roleId
@@ -105,7 +127,7 @@ do
   offset=`expr $limit \* $counter`
 
   psql -d open_lmis -U postgres -t -A -F"," -c "
-  SELECT Q1.username, Q1.facilityId, Q1.programId, Q1.requisitionId, Q2.facilityId, Q2.programId, Q2.requisitionId FROM (
+  SELECT Q1.username, Q1.programId, Q1.requisitionId, Q2.programId, Q2.requisitionId FROM (
     	SELECT U.username, R.facilityId, R.programId, R.id as requisitionId
     	from requisitions R JOIN supervisory_nodes SN ON R.supervisoryNodeId = SN.id
     	JOIN role_assignments RA ON SN.id = RA.supervisoryNodeId
